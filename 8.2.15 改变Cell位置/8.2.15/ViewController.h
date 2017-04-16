8083ff951eb89028ffd663595d334019d1c1d688
test <422538722@qq.com>
2017-04-16 14:23:26 +0800

    8.2.15
    改变Cell位置

---
+++ 8.2.15 改变Cell位置/8.2.15/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  8.2.15
+//
+//  Created by 李维佳 on 2017/4/1.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 8.2.15 改变Cell位置/8.2.15/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  8.2.15
+//
+//  Created by 李维佳 on 2017/4/1.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
